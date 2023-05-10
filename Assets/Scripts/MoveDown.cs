68c30dff751b90479f904a848c9eb480404137ef
John Kjaer <kjaerjohn22@gmail,com>
2023-05-10 16:29:13 +0800

    Implement the New Movement
    made a GameManager to Manage the agility mini game

--- Assets/Scripts/MoveDown.cs
+++ Assets/Scripts/MoveDown.cs
@@ -13,7 +13,7 @@ public class MoveDown : MonoBehaviour
     {
         this.gameObject.transform.position = new Vector3(transform.position.x - 3.0f * Time.deltaTime, transform.position.y,
             transform.position.z);
-        Debug.Log(this.gameObject.transform.position);
+        //Debug.Log(this.gameObject.transform.position);
 
         if(transform.position.x  < -10)
         {
