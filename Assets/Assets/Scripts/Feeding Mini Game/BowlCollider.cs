b23f758aec777e4c0da28a3e56bf40c08de67413
Veo Yap <veo.yap44@gmail.com>
2023-05-17 10:57:04 +0800

    Feeding Mini Game Sound Implementation
    Added SFX for catching food

--- Assets/Assets/Scripts/Feeding Mini Game/BowlCollider.cs
+++ Assets/Assets/Scripts/Feeding Mini Game/BowlCollider.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BowlCollider : MonoBehaviour
 {
     public FeedingGameManager gameManager;
+    public SFXPlayer SFX;
     //public FoodData data;
     public FoodSpawner spawner;
 
@@ -14,12 +15,13 @@ public class BowlCollider : MonoBehaviour
         {
             gameManager.UpdateGoodScore();
             spawner.goodPool.Release(collision.gameObject);
-
+            SFX.PlayGoodAudio();
         }
         else if (collision.gameObject.CompareTag("Bad Food"))
         {
             gameManager.UpdateBadScore();
             spawner.badPool.Release(collision.gameObject);
+            SFX.PlayBadAudio();
         }
         else
         {
