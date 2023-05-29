de56e9b909497ebf3abbac1e88088fd6abc2d6fc
Dylan Armenio <darmenio@amberstudio.com>
2023-05-29 11:53:47 +0800

    UI Update

--- Assets/Assets/Scripts/DataManager/ScriptableObjects/DogSO.cs
+++ Assets/Assets/Scripts/DataManager/ScriptableObjects/DogSO.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 public class DogSO : ScriptableObject
 {
     [SerializeField] public string _dogType;
+    [SerializeField] public string _dogName;
     [SerializeField] public int _obedience;
     [SerializeField] public int _beauty;
     [SerializeField] public int _agility;
     [SerializeField] public int _health;
     [SerializeField] public int _energy;
     [SerializeField] public int _mood;
+    [SerializeField] public Sprite _image;
 }
--- Assets/Assets/Scripts/DataManager/ScriptableObjects/GetDogSO.cs
+++ Assets/Assets/Scripts/DataManager/ScriptableObjects/GetDogSO.cs
@@ -7,12 +7,14 @@ public class GetDogSO : MonoBehaviour
     [SerializeField] public DogSO dogData;
 
     public string _dogType;
+    public string _dogName;
     public int _obedience;
     public int _beauty;
     public int _agility;
     public int _health;
     public int _energy;
     public int _mood;
+    public Sprite _image;
     void Start()
     {
         SetDataOnStart();
@@ -27,11 +29,13 @@ public class GetDogSO : MonoBehaviour
     void SetDataOnStart()
     {
         this._dogType = dogData._dogType;
+        this._dogName = dogData._dogName;
         this._obedience = dogData._obedience;
         this._beauty = dogData._beauty;
         this._agility = dogData._agility;
         this._health = dogData._health;
         this._energy = dogData._energy;
         this._mood = dogData._mood;
+        this._image = dogData._image;
     }
 }
