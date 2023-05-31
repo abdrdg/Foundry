89b9f42fc8ce945f770d3605e3c89706f1ec445b
CarebearDrew <paullopez0230@gmail.com>
2023-05-31 12:28:19 +0800

    Added a EquipBrush function to Grooming Manager

--- Assets/GroomingManager.cs
+++ Assets/GroomingManager.cs
@@ -4,6 +4,7 @@ public class GroomingManager : MonoBehaviour
 {
     public GameObject Soap;
     public GameObject Shower;
+    public GameObject Brush;
     public void EquipSoap()
     {
         Instantiate( Soap,transform.position,Quaternion.identity );
@@ -12,5 +13,9 @@ public class GroomingManager : MonoBehaviour
     public void EquipShower()
     {
         Instantiate(Shower, transform.position, Quaternion.identity);
+    }
+    public void EquipBrush()
+    {
+        Instantiate(Brush, transform.position, Quaternion.identity);
     }
 }
