1889bd272eec69a3fadebc6e3d04937b42e5694b
Dylan Armenio <darmenio@amberstudio.com>
2023-05-31 11:49:29 +0800

    Added Drying feature
    • Added Drying function with assets

--- Assets/Assets/Scripts/Grooming/GroomingManager.cs
+++ Assets/Assets/Scripts/Grooming/GroomingManager.cs
@@ -4,13 +4,19 @@ public class GroomingManager : MonoBehaviour
 {
     public GameObject Soap;
     public GameObject Shower;
+    public GameObject Dryer;
     public void EquipSoap()
     {
-        Instantiate( Soap,transform.position,Quaternion.identity );
+        Instantiate(Soap, transform.position, Quaternion.identity);
     }
 
     public void EquipShower()
     {
         Instantiate(Shower, transform.position, Quaternion.identity);
+    }
+
+    public void EquipDryer()
+    {
+        Instantiate(Dryer, transform.position, Quaternion.identity);
     }
 }
