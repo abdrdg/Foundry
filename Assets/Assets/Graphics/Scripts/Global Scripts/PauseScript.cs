963f6a9d1fa478f0c90c21a1150777a2a0d55426
khmflrdlz <khimflordeliz@gmail.com>
2023-05-31 09:20:31 +0800

    added pause button and pause menu in the agility scene

---
+++ Assets/Assets/Graphics/Scripts/Global Scripts/PauseScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    protected bool isPaused = false;
+    public GameObject prefab;
+    public GameObject pause;
+    public AudioSource _bgm;
+
+    public void OnClick()
+    {
+        if (isPaused == false)
+        {
+            Time.timeScale = 0.0f;
+            _bgm.Pause();
+            isPaused = true;
+
+            Instantiate(prefab,new Vector3(0,0,0),Quaternion.identity);
+            //pauseMenu.transform.SetParent(pause.transform);
+
+            Debug.Log("Paused");
+
+        
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+            _bgm.Play();
+            isPaused = false;
+        }
+    }
+}
