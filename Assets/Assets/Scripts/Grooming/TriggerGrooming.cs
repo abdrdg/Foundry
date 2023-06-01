bac849af3005dd50a78b9bcdebc0859c61cf614f
John Kjaer <kjaerjohn22@gmail,com>
2023-06-01 12:38:00 +0800

    Added a gromming checker and updating the trigger grooming
    added more conditions for trigger grooming and lower the box collider of the soap

--- Assets/Assets/Scripts/Grooming/TriggerGrooming.cs
+++ Assets/Assets/Scripts/Grooming/TriggerGrooming.cs
@@ -1,23 +1,114 @@
+using System.ComponentModel.Design;
 using UnityEngine;
 
 public class TriggerGrooming : MonoBehaviour
 {
     public GameObject dog;
+    public GameObject previousSelected;
+
+    public GroomingChecker gc;
 
     public float _soapingProgression = 0;
     public float _maxSoapingProgression = 100;
     public float _soapSpeed;
 
+    public float _headProgressionStorer;
+    public float _bodyProgressionStorer;
+    public float _legProgressionStorer;
     public void Update()
     {
-        if(_soapingProgression > 0 && _soapingProgression != _maxSoapingProgression )
+        if(dog!= null && dog.name != "Head" )
         {
-            _soapingProgression -= 1 * Time.deltaTime;
+            _bodyProgressionStorer -= 1 * Time.deltaTime;
+            _legProgressionStorer -= 1 * Time.deltaTime;
+
+            if (_bodyProgressionStorer < 0)
+            {
+                _bodyProgressionStorer = 0;
+            }
+
+            if(_legProgressionStorer < 0)
+            {
+                _legProgressionStorer = 0;
+            }
+        }
+       
+        if (dog != null && dog.name != "Body")
+        {
+            _headProgressionStorer -= 1 * Time.deltaTime;
+            _legProgressionStorer -= 1 * Time.deltaTime;
+
+            if (_headProgressionStorer < 0)
+            {
+                _headProgressionStorer = 0;
+            }
+
+            if(_legProgressionStorer < 0)
+            {
+                _legProgressionStorer = 0;
+            }
+
+        }
+       
+        if (dog != null && dog.name != "Legs")
+        {
+            _headProgressionStorer -= 1 * Time.deltaTime;
+            _bodyProgressionStorer -= 1 * Time.deltaTime;
+
+            if (_headProgressionStorer < 0)
+            {
+                _headProgressionStorer = 0;
+            }
+
+            if(_bodyProgressionStorer < 0)
+            {
+                _bodyProgressionStorer = 0;
+            }
         }
 
-        if(_soapingProgression == _maxSoapingProgression )
+        else if( dog == null )
         {
-            Debug.Log("Finish task");
+            _headProgressionStorer -= 1 * Time.deltaTime;
+            _bodyProgressionStorer -= 1 * Time.deltaTime;
+            _legProgressionStorer -= 1 * Time.deltaTime;
+
+            if (_headProgressionStorer < 0)
+            {
+                _headProgressionStorer = 0;
+            }
+
+            if (_bodyProgressionStorer < 0)
+            {
+                _bodyProgressionStorer = 0;
+            }
+
+            if (_legProgressionStorer < 0)
+            {
+                _legProgressionStorer = 0;
+            }
+        }
+
+        if (_soapingProgression == _maxSoapingProgression && gc != null )
+        {
+            if(this.gameObject.name == "Soap(Clone)")
+            {
+               gc.isFinishedSoaping = true;
+            }
+
+            if(this.gameObject.name == "Shower(Clone)")
+            {
+                gc.isFinishedShowering = true;
+            }
+
+            if(this.gameObject.name == "Brush(Clone)")
+            {
+                gc.isFinishedBrushing = true;
+            }
+
+            if(this.gameObject.name == "Dryer(Clone)")
+            {
+                gc.isFinishedDrying = true;
+            }
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -25,24 +116,89 @@ public class TriggerGrooming : MonoBehaviour
         if(collision.gameObject.tag == "Player")
         {
             dog = collision.gameObject;
+            gc = dog.GetComponent<GroomingChecker>();
+        }
+
+        if (dog != null && previousSelected != null && previousSelected != dog)
+        {
+            if (dog.name == "Head")
+            {
+                _soapingProgression = _headProgressionStorer;
+            }
+
+            if (dog.name == "Body")
+            {
+                _soapingProgression = _bodyProgressionStorer;
+            }
+
+            if (dog.name == "Legs")
+            {
+                _soapingProgression = _legProgressionStorer;
+            }
+        }
+
+        if (gc != null)
+        {
+            if (this.gameObject.name == "Soap(Clone)" && gc.isFinishedSoaping)
+            {
+                dog = null;
+                previousSelected = null;
+            }
+
+            if(this.gameObject.name == "Shower(Clone)" && gc.isFinishedShowering)
+            {
+                dog = null;
+                previousSelected = null;
+            }
+
+            if (this.gameObject.name == "Brush(Clone)" && gc.isFinishedBrushing)
+            {
+                dog = null;
+                previousSelected = null;
+            }
+
+            if(this.gameObject.name == "Dryer(Clone)" && gc.isFinishedDrying)
+            {
+                dog = null;
+                previousSelected = null;
+            }    
         }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
+    { 
+        if (dog != null)
         {
-            if (dog != null)
-            {
-                _soapingProgression += _soapSpeed * Time.deltaTime;
+            _soapingProgression += _soapSpeed * Time.deltaTime;
 
-                if(_soapingProgression >= _maxSoapingProgression)
-                {
-                    _soapingProgression = _maxSoapingProgression;
-                    collision.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
-                    _soapingProgression = 0;
-                }
+            if (_soapingProgression >= _maxSoapingProgression)
+            {
+                _soapingProgression = _maxSoapingProgression;
+                _soapingProgression = 100;
+
             }
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if(dog != null)
+        previousSelected = dog;
+       
+
+        if(previousSelected.name == "Head" && previousSelected != null)
+        {
+            _headProgressionStorer = _soapingProgression;
+        }
+
+        else if (previousSelected.name == "Body" && previousSelected != null)
+        {
+            _bodyProgressionStorer = _soapingProgression;
+        }
+
+        else if (previousSelected.name == "Legs" && previousSelected != null)
+        {
+            _legProgressionStorer = _soapingProgression;
         }
     }
 }
