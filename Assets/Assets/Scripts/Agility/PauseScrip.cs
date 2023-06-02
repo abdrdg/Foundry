73f3ea4b9cfce28987473cbe398607f78c77babb
khmflrdlz <khimflordeliz@gmail.com>
2023-06-02 09:35:29 +0800

    implement pause function and created pause menu in the updated dev branch

--- Assets/Assets/Scripts/Agility/PauseScrip.cs
+++ Assets/Assets/Scripts/Agility/PauseScrip.cs
@@ -5,21 +5,20 @@ using UnityEngine;
 public class PauseScrip : MonoBehaviour
 {
     protected bool isPaused = false;
+    public GameObject prefab;
+    public GameObject gotoObject;
+    public GameObject pauseButton;
     public AudioSource _bgm;
+    
 
     public void OnClick()
     {
-        if (isPaused == false)
-        {
             Time.timeScale = 0.0f;
             _bgm.Pause();
             isPaused = true;
-        }
-        else
-        {
-            Time.timeScale = 1.0f;
-            _bgm.Play();
-            isPaused = false;
-        }
+
+            GameObject pauseMenu = Instantiate(prefab,new Vector3(549,262,0),Quaternion.identity);
+            pauseMenu.transform.SetParent(gotoObject.transform);
+            pauseButton.SetActive(false);
     }
 }
