c2a9a466a04f5398be3114cafd5f8de099616651
khmflrdlz <khimflordeliz@gmail.com>
2023-06-14 17:34:53 +0800

    used sliced asset dog in the Grooming Scene

--- Assets/Assets/Scripts/Grooming/DirtHolder.cs
+++ Assets/Assets/Scripts/Grooming/DirtHolder.cs
@@ -5,4 +5,22 @@ using UnityEngine;
 public class DirtHolder : MonoBehaviour
 {
     public List<GameObject> Dirt = new List<GameObject>();
+    public SpriteRenderer spriteRenderer;
+    public Sprite WetSprite;
+    public Sprite PoofySprite;
+
+    public void Update()
+    {
+        if(Dirt.Count == 0)
+        {
+            ChangeSprite();
+        }
+
+    }
+
+    public void ChangeSprite()
+    {
+       
+            spriteRenderer.sprite = WetSprite;
+    }
 }
