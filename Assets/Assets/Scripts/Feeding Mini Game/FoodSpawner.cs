0c17800bc5663e8b4fd9f57253650d43bb6694a6
Dylan Armenio <darmenio@amberstudio.com>
2023-06-15 16:17:56 +0800

    Number of Foods Falling has been adjusted
    - Change script on spawnTimeInterval

--- Assets/Assets/Scripts/Feeding Mini Game/FoodSpawner.cs
+++ Assets/Assets/Scripts/Feeding Mini Game/FoodSpawner.cs
@@ -13,15 +13,16 @@ public class FoodSpawner : MonoBehaviour
     private int goodFoodWeight = 1;
     private int badFoodWeight = 1;
 
-    [SerializeField] private int spawnTimeInterval = 3;
+    [SerializeField] private float spawnTimeInterval = 0.4f;
+
     // Start is called before the first frame update
     void Start()
     {
         Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(UnityEngine.Random.Range(0.1f, 0.9f), 4.36f, 0));
         pos.z = 0.0f;
 
-        goodPool = CreatePool(pos, foods[1], 1, 2);
-        badPool = CreatePool(pos, foods[0], 1, 2);
+        goodPool = CreatePool(pos, foods[1], 1, 8);
+        badPool = CreatePool(pos, foods[0], 1, 8);
 
         StartCoroutine(Spawner());
     }
@@ -40,11 +41,10 @@ public class FoodSpawner : MonoBehaviour
     {
         Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(UnityEngine.Random.Range(0.1f, 0.9f), 4.36f, 0));
         pos.z = 0.0f;
-        
 
         int totalWeight = goodFoodWeight + badFoodWeight;
         var randomFood = Random.Range(0, totalWeight - 1);
-                
+
         if (randomFood < goodFoodWeight)
         {
             badFoodWeight++;
@@ -62,25 +62,21 @@ public class FoodSpawner : MonoBehaviour
     public ObjectPool<GameObject> CreatePool(Vector3 pos, GameObject Objectprefab, int minAmount, int maxAmount)
     {
         var pool = new ObjectPool<GameObject>(() =>
-         {
-             return Instantiate(Objectprefab, pos, Quaternion.identity);
-         }, food =>
-         {
-             food.gameObject.SetActive(true);
+        {
+            return Instantiate(Objectprefab, pos, Quaternion.identity);
+        }, food =>
+        {
+            food.gameObject.SetActive(true);
 
-         }, food =>
-         {
-             
-             food.gameObject.SetActive(false);
-             
-         }, food =>
-         {
-             Destroy(food);
-         }, false, minAmount, maxAmount);
+        }, food =>
+        {
+            food.gameObject.SetActive(false);
+
+        }, food =>
+        {
+            Destroy(food);
+        }, false, minAmount, maxAmount);
 
         return pool;
     }
-
-
-
 }
