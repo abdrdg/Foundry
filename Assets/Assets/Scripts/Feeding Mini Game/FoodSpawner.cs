e17d4d4b3fedbbd94d65d93ce40302e7cad4bed8
Dylan Armenio <darmenio@amberstudio.com>
2023-06-15 17:17:48 +0800

    Food Spawn Interval Adjustment
    - Added a random spawn of Food between 1 or 2
    - Added a delay between two food spawning

--- Assets/Assets/Scripts/Feeding Mini Game/FoodSpawner.cs
+++ Assets/Assets/Scripts/Feeding Mini Game/FoodSpawner.cs
@@ -31,33 +31,74 @@ public class FoodSpawner : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(spawnTimeInterval);
+            yield return new WaitForSeconds(UnityEngine.Random.Range(spawnTimeInterval * 0.5f, spawnTimeInterval * 1.5f));
             foodBowl.ChangeAnim();
-            CreateItems();
+
+            int numFoodToSpawn = UnityEngine.Random.Range(1, 3); // Randomly determine the number of food items to spawn (either 1 or 2)
+
+            for (int i = 0; i < numFoodToSpawn; i++)
+            {
+                CreateItems();
+                yield return new WaitForSeconds(UnityEngine.Random.Range(0.1f, 0.3f)); // Delay between spawning each food item
+            }
         }
     }
+
 
     private void CreateItems()
+{
+    Vector3 pos1 = Camera.main.ViewportToWorldPoint(new Vector3(UnityEngine.Random.Range(0.1f, 0.9f), 1.36f, 0));
+    pos1.z = 0.0f;
+
+    Vector3 pos2 = Camera.main.ViewportToWorldPoint(new Vector3(UnityEngine.Random.Range(0.1f, 0.9f), 1.36f, 0));
+    pos2.z = 0.0f;
+
+    int totalWeight = goodFoodWeight + badFoodWeight;
+    var randomFood1 = Random.Range(0, totalWeight - 1);
+    var randomFood2 = Random.Range(0, totalWeight - 1);
+
+    if (randomFood1 < goodFoodWeight)
     {
-        Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(UnityEngine.Random.Range(0.1f, 0.9f), 4.36f, 0));
-        pos.z = 0.0f;
-
-        int totalWeight = goodFoodWeight + badFoodWeight;
-        var randomFood = Random.Range(0, totalWeight - 1);
-
-        if (randomFood < goodFoodWeight)
-        {
-            badFoodWeight++;
-            var goodfood = goodPool.Get();
-            goodfood.transform.position = pos;
-        }
-        else
-        {
-            goodFoodWeight++;
-            var badfood = badPool.Get();
-            badfood.transform.position = pos;
-        }
+        badFoodWeight++;
+        var goodfood = goodPool.Get();
+        goodfood.transform.position = pos1;
     }
+    else
+    {
+        goodFoodWeight++;
+        var badfood = badPool.Get();
+        badfood.transform.position = pos1;
+    }
+
+    if (randomFood2 < goodFoodWeight)
+    {
+        badFoodWeight++;
+        StartCoroutine(DelayedSpawn(pos2, true));
+    }
+    else
+    {
+        goodFoodWeight++;
+        StartCoroutine(DelayedSpawn(pos2, false));
+    }
+}
+
+private IEnumerator DelayedSpawn(Vector3 position, bool spawnGoodFood)
+{
+    yield return new WaitForSeconds(0.3f);
+
+    if (spawnGoodFood)
+    {
+        var goodfood = goodPool.Get();
+        goodfood.transform.position = position;
+    }
+    else
+    {
+        var badfood = badPool.Get();
+        badfood.transform.position = position;
+    }
+}
+
+
 
     public ObjectPool<GameObject> CreatePool(Vector3 pos, GameObject Objectprefab, int minAmount, int maxAmount)
     {
