622ba5779635414a738f6b451e9b31dca00c5a27
John Kjaer <kjaerjohn22@gmail,com>
2023-06-29 18:00:00 +0800

    Added selection manager in main menu

--- Assets/Assets/Scripts/Play Park Scripts/PanelClose.cs
+++ Assets/Assets/Scripts/Play Park Scripts/PanelClose.cs
@@ -6,10 +6,13 @@ public class PanelClose : MonoBehaviour
     public GameObject panelToClose;
     public GameObject panelToOpen;
     public GameObject dog;
+    public SelectionManager sm;
+
     private Button button;
 
     private void Start()
     {
+        dog = sm._dogSelected;
         button = GetComponent<Button>();
         button.onClick.AddListener(SwitchPanels);
     }
--- Assets/Assets/Scripts/Play Park Scripts/StatsDisplay.cs
+++ Assets/Assets/Scripts/Play Park Scripts/StatsDisplay.cs
@@ -6,7 +6,8 @@ using TMPro;
 
 public class StatsDisplay : MonoBehaviour
 {
-    public DogSO dog;
+    public GetDogSO gds;
+    public SelectionManager _sm;
 
     public TextMeshProUGUI dogtypeTMP_Text;
     public TextMeshProUGUI dognameTMP_Text;
@@ -15,15 +16,21 @@ public class StatsDisplay : MonoBehaviour
     public TextMeshProUGUI obedienceTMP_Text;
 
     public Image artworkImage;
-
+                            
     void Start()
     {
-        dogtypeTMP_Text.text = dog._dogType; 
-        dognameTMP_Text.text = dog._dogName; 
-        agilityTMP_Text.text = "Agility: " + dog._agility.ToString(); 
-        beautyTMP_Text.text = "Beauty: " + dog._beauty.ToString(); 
-        obedienceTMP_Text.text = "Obedience: " + dog._obedience.ToString(); 
+         gds = _sm._dogSelected.GetComponent<GetDogSO>();
 
-        artworkImage.sprite = dog._image;
+        dogtypeTMP_Text.text = gds._dogType;
+        dognameTMP_Text.text = gds._dogName;
+        agilityTMP_Text.text = "Agility: " + gds._agility.ToString();
+        beautyTMP_Text.text = "Beauty: " + gds._beauty.ToString();
+        obedienceTMP_Text.text = "Obedience: " + gds._obedience.ToString();
+        //artworkImage.sprite = dog._image;
+    }
+
+    private void Update()
+    {
+       
     }
 }
